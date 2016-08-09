c3e58d954cfc67622ac0fc01343740733461f79d
Xavier Lian <xavierliancw@gmail.com>
2016-08-09 02:17:56 -0700

    Closes #11

--- src/Player.java
+++ src/Player.java
@@ -30,6 +30,7 @@ public class Player
 			
 			choice = sc.nextLine();
 			choice = choice.toLowerCase();
+			choice = choice.trim();
 			
 			//Check to see if the verb exists in the verb bank
 			for (int x = 0; x < verbBank.length; x++)
@@ -66,9 +67,10 @@ public class Player
 		do
 		{
 			System.out.print("Should I move (forward), (back), (left), "
-					+ "(right), or should I (stay)? ");
+					+ "(right), or should I (stay)?\n");
 			input = sc.nextLine();
 			input = input.toLowerCase();
+			input = input.trim();
 			
 			//Check if input is valid
 			if (input.equals("forward") ||input.equals("back") 
@@ -82,76 +84,81 @@ public class Player
 				System.out.print("I can't move like that.\n");
 			}
 			//Check if a room is available to move to
-			switch (input)
+			if (pass)
 			{
-			case "forward":
-				//If the array location exists, the room exists, and not
-				//locked
-				if (y + 1 < map[x].length && map[x][y + 1].exists()
-					&& !map[x][y + 1].locked())
+				switch (input)
 				{
-					y += 1;
+				case "forward":
+					//If the array location exists, the room exists, and not
+					//locked
+					if (y + 1 < map[x].length && map[x][y + 1].exists()
+						&& !map[x][y + 1].locked())
+					{
+						y += 1;
+					}
+					else if (y + 1 < map[x].length 
+							 && map[x][y + 1].locked())
+					{
+						System.out.print("The door's locked.\n");
+					}
+					else
+					{
+						System.out.print("There's no door that way.\n");
+					}
+					break;
+				case "back":
+					if (y - 1 > -1 && map[x][y - 1].exists()
+						&& !map[x][y - 1].locked())
+					{
+						y -= 1;
+					}
+					else if (y - 1 > -1 && map[x][y - 1].locked())
+					{
+						System.out.print("The door's locked.\n");
+					}
+					else
+					{
+						System.out.print("There's no door that way.\n");
+					}
+					break;
+				case "left":
+					if (x - 1 > -1 && map[x - 1][y].exists()
+						&& !map[x - 1][y].locked())
+					{
+						x -= 1;
+					}
+					else if (x - 1 > -1 && map[x - 1][y].locked())
+					{
+						System.out.print("The door's locked.\n");
+					}
+					else
+					{
+						System.out.print("There's no door that way.\n");
+					}
+					break;
+				case "right":
+					if (x + 1 < map[x].length && map[x + 1][y].exists()
+						&& !map[x + 1][y].locked())
+					{
+						x += 1;
+					}
+					else if (x + 1 < map[x].length 
+							 && map[x + 1][y].locked())
+					{
+						System.out.print("The door's locked.\n");
+					}
+					else
+					{
+						System.out.print("There's no door that way.\n");
+					}
+					break;
+				case "stay":
+					System.out.print("Maybe I won't move after all.\n");
+					break;
+				default:
+					System.out.print("\tPlayer::movePrompt(): Critical "
+							+ "movement switch default\n");
 				}
-				else if (map[x][y + 1].locked())
-				{
-					System.out.print("The door's locked.\n");
-				}
-				else
-				{
-					System.out.print("There's no door that way.\n");
-				}
-				break;
-			case "back":
-				if (y - 1 < map[x].length && map[x][y - 1].exists()
-					&& !map[x][y - 1].locked())
-				{
-					y -= 1;
-				}
-				else if (map[x][y - 1].locked())
-				{
-					System.out.print("The door's locked.\n");
-				}
-				else
-				{
-					System.out.print("There's no door that way.\n");
-				}
-				break;
-			case "left":
-				if (x - 1 < map[x].length && map[x - 1][y].exists()
-					&& !map[x - 1][y].locked())
-				{
-					x -= 1;
-				}
-				else if (map[x - 1][y].locked())
-				{
-					System.out.print("The door's locked.\n");
-				}
-				else
-				{
-					System.out.print("There's no door that way.\n");
-				}
-				break;
-			case "right":
-				if (x + 1 < map[x].length && map[x + 1][y].exists()
-					&& !map[x + 1][y].locked())
-				{
-					x += 1;
-				}
-				else if (map[x + 1][y].locked())
-				{
-					System.out.print("The door's locked.\n");
-				}
-				else
-				{
-					System.out.print("There's no door that way.\n");
-				}
-				break;
-			case "stay":
-				System.out.print("Maybe I won't move after all.\n");
-				break;
-			default:
-				System.out.print("\tPlayer::movePrompt(): Critical "
-						+ "movement switch default\n");
 			}
 		} while (!pass);
 	}
